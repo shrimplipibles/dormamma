b6b6887d99c9e3e605d3f46174e24d1c6585c534
shrimplipibles <50250686+shrimplipibles@users.noreply.github.com>
2019-05-31 19:12:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,12 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
+client.on('ready', () => {
+    // List servers the bot is connected to
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+    })
+})
+
+client.login("NTg0MDU2NjE0NzE0MjEyMzkx.XPFbyg.V1f6CrMPF0ia6hpRNMonGp1MKEE")
